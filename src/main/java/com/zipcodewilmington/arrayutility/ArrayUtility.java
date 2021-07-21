6e038f22af8abd06c629cd1e868086b431c0a7cd
sitarameez <sitarablossom@gmail.com>
2021-07-20 22:09:06 -0400

    did some

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,34 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<E> {
+    E[] inputArray;
+
+    public ArrayUtility(E[] inputArray){
+        this.inputArray=inputArray;
+    }
+
+    public Integer getNumberOfOccurrences(E valueToEvaluate){
+        int count =0;
+        for (E element:inputArray
+             ) {
+            if(element==valueToEvaluate) {
+                count++;
+            }
+        }
+        return count;
+            }
+
+public E[] removeValue(E valueToRemove){
+return null;
+}
+
+public Integer countDuplicatesInMerge(E[] arrayToMerge,E valueToEvaluate){
+return null;
+}
+
+public E getMostCommonFromMerge(E[] arrayToMerge){
+        return null;
+
+}
 }
