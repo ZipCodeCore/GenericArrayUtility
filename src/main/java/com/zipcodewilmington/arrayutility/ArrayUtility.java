2f7ebf1e84270d475871549fb9dca9b3e0b89fdf
MannyMb <emmanuelnefo98@gmail.com>
2021-07-28 00:53:43 -0400

    saving progress

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,17 +1,20 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+
 /**
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility<SomeType> {
 
-    private final SomeType[] array;
+    private SomeType[] array;
 
     public ArrayUtility(SomeType[] array) {
         this.array = array;
     }
 
     public Integer countDuplicatesInMerge(SomeType[] arrayToMerge, SomeType valueToEvaluate) {
+
         return null;
     }
 
@@ -20,10 +23,23 @@ public class ArrayUtility<SomeType> {
     }
 
     public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
-        return null;
+        int tracker = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(valueToEvaluate.equals(array[i])) {
+                tracker++;
+            }
+        }
+        return tracker;
     }
 
     public SomeType[] removeValue(SomeType valueToRemove) {
-        return null;
+        ArrayList<SomeType> arrayList = new ArrayList<>();
+        for (Object element : array) {
+            if (!element.equals(valueToRemove)) {
+                arrayList.add((SomeType) element);
+            }
+
+        }
+        return (SomeType[]) arrayList.toArray();
     }
 }
