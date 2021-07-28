2bf8a6509890084e7ce66ebad06d7d9b67e3a426
MannyMb <emmanuelnefo98@gmail.com>
2021-07-28 19:07:54 -0400

    finished

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,8 +1,6 @@
 package com.zipcodewilmington.arrayutility;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by leon on 3/6/18.
@@ -15,20 +13,47 @@ public class ArrayUtility<SomeType> {
         this.array = array;
     }
 
-    public Integer countDuplicatesInMerge(SomeType[] arrayToMerge, SomeType valueToEvaluate) {
-        List<SomeType> mergedArray = new ArrayUtility<SomeType>();
-        Collections.addAll(mergedArray, array );
-        Collections.addAll(mergedArray, arrayToMerge);
+    public List<SomeType> mergeArray(SomeType[] arrayToMerge){
+        List<SomeType> mergedArray = new ArrayList<>();
+        for (SomeType s : arrayToMerge) {
+            mergedArray.add(s);
+        }
+        for (SomeType s : this.array) {
+            mergedArray.add(s);
+        }
+        return mergedArray;
+    }
 
-        return null;
+    public Integer countDuplicatesInMerge(SomeType[] arrayToMerge, SomeType valueToEvaluate) {
+        List<SomeType> mergedArray = mergeArray(arrayToMerge);
+        int counter = 0;
+        for (SomeType something: mergedArray) {
+            if (something.equals(valueToEvaluate)){
+                counter++;
+            }
+        }
+        return counter;
     }
 
     public SomeType getMostCommonFromMerge(SomeType[] arrayToMerge) {
-        List<SomeType> mergedArray = new ArrayUtility<SomeType>();
-        Collections.addAll(mergedArray, array );
-        Collections.addAll(mergedArray, arrayToMerge);
-
-        return null;
+        List<SomeType> mergedArray = mergeArray(arrayToMerge);
+        Map<SomeType, Integer> myMap = new HashMap<>();
+        for (SomeType s : mergedArray) {
+            if (myMap.containsKey(s)) {
+                myMap.put(s, myMap.get(s) + 1);
+            } else {
+                myMap.put(s, 1);
+            }
+        }
+        int mostCommon = 0;
+        SomeType theType = null;
+        for (Map.Entry<SomeType, Integer> a: myMap.entrySet()){
+            if (a.getValue() > mostCommon){
+                mostCommon = a.getValue();
+                theType = a.getKey();
+            }
+        }
+        return theType;
     }
 
     public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
@@ -43,12 +68,15 @@ public class ArrayUtility<SomeType> {
 
     public SomeType[] removeValue(SomeType valueToRemove) {
         ArrayList<SomeType> arrayList = new ArrayList<>();
-        for (Object element : array) {
+        for (SomeType element : array) {
             if (!element.equals(valueToRemove)) {
-                arrayList.add((SomeType) element);
+                arrayList.add(element);
             }
-
         }
-        return (SomeType[]) arrayList.toArray();
+        SomeType[] newArray = (SomeType[]) java.lang.reflect.Array.newInstance(array[0].getClass(),array.length);
+        for (int i = 0; i < arrayList.size(); i++) {
+            newArray[i] = arrayList.get(i);
+        }
+        return newArray;
     }
 }
