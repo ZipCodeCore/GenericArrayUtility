e94324320192b68ed0c8891702b355255f44501c
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-12-04 15:31:06 -0500

    gave skeleton methods

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,27 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<E> {
+    E[] array;
+    public ArrayUtility(E[] array){
+        this.array = array;
+    }
+
+    public E[] removeValue(E valueToRemove) {
+        return (array = Arrays.stream(this.array)
+                .filter(q-> !q.equals(valueToRemove))
+                .toArray([]::new));
+    }
+
+    public E countDuplicatesInMerge(E[] arrayToMerge, E valueToEvaluate) {
+    }
+
+    public E getNumberOfOccurrences(E valueToEvaluate) {
+    }
 }
