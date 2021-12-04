e669a211c1b6a97c9b4e4032f4739c47332070cb
Quatrani Paul <quatranipaul@zipcodes-MacBook-Pro-9.local>
2021-12-04 17:39:32 -0500

    passes all tests

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,8 +1,8 @@
 package com.zipcodewilmington.arrayutility;
 
-import java.util.ArrayList;
+import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Comparator;
 
 /**
  * Created by leon on 3/6/18.
@@ -15,30 +15,33 @@ public class ArrayUtility<E> {
 //is it posssible to do this better without an unchecked cast?
     //maybe doing my own array conversion method?
     public E[] removeValue(E valueToRemove) {
-        return (this.array = (E[]) Arrays.stream(this.array)
+        return Arrays
+                .stream(this.array)
                 .filter(q-> !q.equals(valueToRemove))
-                .toArray());
+                .toArray(q ->  Arrays.copyOf(this.array,
+                                this.array.length - getNumberOfOccurrences(valueToRemove)));
     }
 
-    public int countDuplicatesInMerge(E[] arrayToMerge, E valueToEvaluate) {
+    public Integer countDuplicatesInMerge(E[] arrayToMerge, E valueToEvaluate) {
         return Math
                 .toIntExact(Arrays
                         .stream(arrayToMerge)
-                        .filter(q-> !q.equals(valueToEvaluate))
-                        .count());
+                        .filter(q-> q.equals(valueToEvaluate))
+                        .count() + getNumberOfOccurrences(valueToEvaluate));
     }
 
-    public int getNumberOfOccurrences(E valueToEvaluate) {
+    public Integer getNumberOfOccurrences(E valueToEvaluate) {
         return Math
                 .toIntExact(Arrays
                         .stream(this.array)
-                        .filter(q-> !q.equals(valueToEvaluate))
+                        .filter(q-> q.equals(valueToEvaluate))
                         .count());
     }
 
-    public E getMostCommonFromMerge(Integer[] arrayToMerge) {
+    public E getMostCommonFromMerge(E[] arrayToMerge) {
         return Arrays
-                .stream(this.array)
-                .max()
+                .stream(arrayToMerge)
+                .max(Comparator.comparing(this::getNumberOfOccurrences))
+                .orElse(null);
     }
 }
